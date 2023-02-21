3cddce0f29d02d4b22e522de78e1f033ada76f4b
StepinaHelen <stopinahelen@gmail.com>
2023-02-21 11:35:18 +0100

    Add dotenv

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const config = require("./config/db");
 const account = require("./routes/account");
 const session = require("express-session");
 const Post = require("./models/post");
+require("dotenv").config();
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT;
 app.use(session({ secret: config.secret }));
 
 app.use(passport.initialize());
